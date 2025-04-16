4a0ab85dc3a5d4872c872f3dca6d61ac690cc247
wellington.defassio <well.defassio@gmail.com>
2025-04-16 20:25:29 -0300

    grpc client

---
+++ src/main/java/io/defassio/sec06/GrpcClient.java
@@ -0,0 +1,33 @@
+package io.defassio.sec06;
+
+import io.defassio.proto.models.sec06.AccountBalance;
+import io.defassio.proto.models.sec06.BalanceCheckRequest;
+import io.defassio.proto.models.sec06.BankServiceGrpc;
+import io.defassio.sec04.Lec01Import;
+import io.grpc.ManagedChannelBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GrpcClient {
+
+    private static final Logger log = LoggerFactory.getLogger(GrpcClient.class);
+
+    public static void main(String[] args) {
+
+        var channel = ManagedChannelBuilder.forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+
+        var stub = BankServiceGrpc.newBlockingStub(channel);
+
+        AccountBalance accountBalance = stub.getAccountBalance(
+                BalanceCheckRequest
+                        .newBuilder()
+                        .setAccountNumber(2)
+                        .build());
+
+
+        log.info("the account balance is {}", accountBalance);
+    }
+
+}
